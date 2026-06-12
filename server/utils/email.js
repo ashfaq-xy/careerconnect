496474f468af7c2b0313342c0e1d3bb6cc682afb
Ashfaq <ashfaq@ashfaq-2.local>
2026-06-12 21:14:21 +0530

    Fix: email port 465 for Render

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,18 +3,14 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: Number(process.env.EMAIL_PORT) || 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-/**
- * Send an email
- * @param {Object} options - { to, subject, html, text }
- */
 exports.sendEmail = async ({ to, subject, html, text }) => {
   const mailOptions = {
     from: `"CareerConnect" <${process.env.EMAIL_FROM || process.env.EMAIL_USER}>`,
