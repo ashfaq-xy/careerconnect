94a9a2c0fbea6b671414136f14603ecf865e08d9
Ashfaq <ashfaq@ashfaq-2.local>
2026-06-12 21:32:05 +0530

    Fix: use Gmail service instead of SMTP

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,19 +1,16 @@
-// server/utils/email.js
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
 exports.sendEmail = async ({ to, subject, html, text }) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
   const mailOptions = {
-    from: `"CareerConnect" <${process.env.EMAIL_FROM || process.env.EMAIL_USER}>`,
+    from: `"CareerConnect" <${process.env.EMAIL_USER}>`,
     to,
     subject,
     html,
